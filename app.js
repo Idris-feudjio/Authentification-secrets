6358851f4c8e079bde1aafd6cfd20b1e4d0323a7
Idris Feudjio <idrisfeudjio8@gmail.com>
2022-06-06 13:31:20 +0100

    realisation de la logique pour le register et le login

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const bodyParser = require("body-parser"); 
+const bodyParser = require("body-parser");
+const res = require("express/lib/response");
 const app = express();
 
 app.use(express.static('public'));
@@ -11,26 +12,67 @@ app.set('view engine', 'ejs');
 mongoose.connect('mongodb://localhost:27017/authentificationDB');
 
 const userSchema = mongoose.Schema({
-    userName : String,
-    password : String
+    userName: {
+        type: String,
+        required: [true, 'username is required']
+    },
+    password: String
 });
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
+
     res.render('home');
 })
 
-app.get('/login', (req, res) => {
-    res.render('login');
-})
+//////////////////////////////// LOGIN ///////////////////////////
+app.route('/login')
+    .get((req, res) => {
+        res.render('login', { noteMatch: '' });
+    })
+    .post((req, res) => { 
+        const username = req.body.username;
+        const password = req.body.password;
+        User.findOne({ username: username }, (err, user) => {
+            if (err) {
+                console.log('USER NOT FOUND');
+            } else {
+                if (user.userName === username && user.password === password) {
+                    res.render('secrets');
+                } else { 
+                    console.log('USERNAME OR PASSWORD INCORRECT'); 
+                    res.render('login', { noteMatch: 'username or password incorrect' }); 
+                }
+            }
+        })
+    })
 
-app.get('/register', (req, res) => {
-    res.render('register');
-})
+//////////////////////////////// USER REGISTER ///////////////////////////
+app.route('/register')
+    .get((req, res) => {
+        res.render('register');
+    })
+    .post((req, res) => {
+        const newUser = new User({
+            userName: req.body.username,
+            password: req.body.password
+        });
+
+        newUser.save((err) => {
+            if (!err) {
+                console.log('NEW USER SAVE SUCCEFULLY');
+            } else {
+                console.log(err);
+            }
+        })
+        res.render('secrets');
+    })
 
 app.get('/secrets', (req, res) => {
     res.render('secrets');
 })
+
+
 
 app.listen(3000, () => {
     console.log('server start at port 3000');
